4d1ba01d7d1930629b9d0daaa128b81fa485afd4
missisol <missisololol@gmail.com>
2019-03-07 18:46:19 +0300

    Switch with routes added

--- src/app.jsx
+++ src/app.jsx
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
 import ReactDom from 'react-dom';
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Menu from 'components/Menu';
 import menuItems from './menuItems';
+import routes from './routes';
 
 class App extends Component {
   render() {
     return (
       <div className="container">
         <Menu items={menuItems}/>
+        <Switch>
+          {routes.map((rout, idx) => <Route key={idx} {...rout} />)}
+        </Switch>
       </div>
     )
   }
